acd83967819693fabb4ab41b3c7f0408c7180798
ansta1994 <anton.stahlgren@hotmail.com>
2024-08-13 15:54:49 +0200

    13/8: - Done with the Scrabble and all the Tests passed

--- src/main/java/com/booleanuk/Scrabble.java
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -1,12 +1,32 @@
 package com.booleanuk;
 
 public class Scrabble {
-    public Scrabble(String word) {
 
+    private final String word;
+    public Scrabble(String word) {
+        this.word = word;
     }
 
     public int score() {
-        return -1;
-    }
+        int points = 0;
 
+        for (int i = 0; i < word.length(); i++) {
+            if ("aAeEiIoOuUlLnNrRsStT".contains("" + word.charAt(i))) {
+                points ++;
+            } else if ("dDgG".contains("" + word.charAt(i))) {
+                points +=2;
+            } else if ("bBcCmMpP".contains("" + word.charAt(i))) {
+                points +=3;
+            } else if ("fFhHvVwWyY".contains("" + word.charAt(i))) {
+                points +=4;
+            } else if ("kK".contains("" + word.charAt(i))) {
+                points +=5;
+            } else if ("jJxX".contains("" + word.charAt(i))) {
+                points +=8;
+            } else if ("qQzZ".contains("" + word.charAt(i))) {
+                points +=10;
+            }
+        }
+        return points;
+    }
 }
